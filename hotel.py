3da2017b1e423085d6d1cd202f47c0b513cf19f1
zsljoe <zsljoe@gmail.com>
2023-11-30 18:50:04 +0100

    hetedik_commit

--- hotel.py
+++ hotel.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 
+
 class Szoba(ABC):
     def __init__(self, szobaszam, ar):
         self.szobaszam = szobaszam
@@ -7,25 +8,34 @@ class Szoba(ABC):
 
 
 class EgyagyasSzoba(Szoba):
-    def __init__(self,szobaszam, ar):
-        szobaszam = 101
-        ar = 30000
+    def __init__(self, szobaszam, ar):
+        self.szobaszam = 101
+        self.ar = 30000
+
 
 class KetagyasSzoba(Szoba):
-    def __init__(self,szobaszam, ar):
-        szobaszam = 102
-        ar = 40000
+    def __init__(self, szobaszam, ar):
+        self.szobaszam = 102
+        self.ar = 40000
+
+
 class Szalloda:
 
-    def __init__(self, name: str):
+    def __init__(self, szobak, name: str):
+        self.szobak = []
         self.name = name
+
+    def szoba_hozzaadas(self, szoba: Szoba):
+        self.szobak.append(szoba)
+
+    def szobakletrehozasa(self):
+        self.szoba_hozzaadas(EgyagyasSzoba)
+        self.szoba_hozzaadas(KetagyasSzoba)
 
 
 class Foglalas:
     def __str__(self):
         foglalasok_str = self.foglalas_datumok()
-
-Szalloda = Szalloda("Toparti")
-
+        return f"Fogalalás dátuma: {foglalasok_str}"
 
 
