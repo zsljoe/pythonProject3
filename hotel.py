408701aa9423a5732210ac1f2966788457134140
zsljoe <zsljoe@gmail.com>
2023-11-30 18:05:04 +0100

    otodik_commit

--- hotel.py
+++ hotel.py
@@ -1,12 +1,35 @@
-class Szoba:
-    pass
+from abc import ABC, abstractmethod
+
+
+class Szoba(ABC):
+
+    def __init__(self, szobaszam, ar):
+        self.szobaszam = szobaszam
+        self.ar = ar
+
 
 class EgyagyasSzoba(Szoba):
     pass
 
+
 class KetagyasSzoba(Szoba):
     pass
 
+
 class Szalloda:
-    pass
+
+    def __init__(self, name: str):
+        self.name = name
+
+    def __str__(self):
+        return f"A szálloda neve {self.name}"
+
+
+Elso_szalloda = Szalloda("Toparti")
+
+print(Elso_szalloda)
+
+
+
+
 
